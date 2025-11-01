e565e0d7d34cca922b7970cac7e86cdedaf0a1eb
Marcelo Vieytes <mvieytes@gmail.com>
2025-11-01 20:27:37 -0300

    Elimina proyecto 70_simple_web_server.

--- 70_simple_web_server/html.h
+++
@@ -1,7 +0,0 @@
-#ifndef _HTML_H_
-#define _HTML_H_
-
-const char* html_page(int* len);
-void html_do_actions(char* request);
-
-#endif
--- 70_simple_web_server/simple_web_server/simple_ws.h
+++
@@ -1,9 +0,0 @@
-#ifndef _SIMPLE_WEB_SERVER_H_
-#define _SIMPLE_WEB_SERVER_H_
-
-#include "wifi.h"
-#include "http.h"
-#include "web_server.h"
-#include "html.h"
-
-#endif
